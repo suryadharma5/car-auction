86434f0893168e8df789b8d2a5c341582c26b6a1
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-05-11 15:04:57 +0700

    feat: create user service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // ini agar tidak ada field tambahan yang disimpan diluar DTO
+      whitelist: true
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
